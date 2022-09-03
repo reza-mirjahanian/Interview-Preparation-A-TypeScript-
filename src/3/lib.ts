3b0f2b5b2bddf658ffc62033168f4799a52805dc
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-03 20:28:10 +0430

    3

--- src/3/lib.ts
+++ src/3/lib.ts
@@ -2,7 +2,9 @@
 
 export default function func(s: string): number {
   const table = new Map();
-  let max = 0, i = 0, j = 0;
+  let max = 0,
+    i = 0,
+    j = 0;
   while (j < s.length) {
     i = table.has(s[j]) ? Math.max(table.get(s[j]) + 1, i) : i;
     table.set(s[j], j++);
--- test/3/lib.spec.ts
+++ test/3/lib.spec.ts
@@ -4,7 +4,7 @@ import func from '../../src/3/lib';
 
 suite('Testing 3 ...', () => {
   test('case 1', async () => {
-    expect(func("abcabcbb")).to.deep.equal(3);
-    expect(func("bbbbb")).to.deep.equal(1);
+    expect(func('abcabcbb')).to.deep.equal(3);
+    expect(func('bbbbb')).to.deep.equal(1);
   });
 });
