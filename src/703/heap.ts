11f9288060138d657594041a03e65d2d53c1a2fc
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-03 10:33:24 +0430

    703

--- src/703/heap.ts
+++ src/703/heap.ts
@@ -1,21 +1,21 @@
 export default class MinHeap {
     // Min heaps can be represented as arrays. We can calculate an index's parent,
     // left and right child via simple math.
-    array: number[];
-    maxSize: number;
+    numbers: number[];
+    MAX_SIZE: number;
 
     constructor(maxSize: number) {
-        this.array = [];
-        this.maxSize = maxSize;
+        this.numbers = [];
+        this.MAX_SIZE = maxSize;
     }
 
     // Add the new value to the end of the array/tree. Heapify up. If our new heap is over capacity,
     // Remove the min value.
     insert(value: number) {
-        this.array.push(value);
+        this.numbers.push(value);
         this.minHeapifyUp();
 
-        if (this.array.length > this.maxSize) {
+        if (this.numbers.length > this.MAX_SIZE) {
             this.extract()
         }
     }
@@ -23,10 +23,10 @@ export default class MinHeap {
     // Remove the first (min) value from the array/tree. Move the last value to the front,
     // and heapify down.
     extract(): number {
-        const min = this.array[0];
-        const lastNode = this.array.pop();
+        const min = this.numbers[0];
+        const lastNode = this.numbers.pop();
 
-        this.array[0] = lastNode!;
+        this.numbers[0] = lastNode!;
         this.minHeapifyDown();
 
         return min;
@@ -34,16 +34,16 @@ export default class MinHeap {
 
     // Retrieve the min value without removing it.
     peek(): number {
-        return this.array[0];
+        return this.numbers[0];
     }
 
     // Start from the last value in the array/tree. Compare it to its parent. If parent value is greater
     // than the current, it does not satisfy the min heap requirement (parent is less than either of
     // its children). If so, swap the values and move up the tree. Otherwise, we're done.
     minHeapifyUp() {
-        for (let ind = this.array.length - 1; ind > 0;) {
+        for (let ind = this.numbers.length - 1; ind > 0;) {
             const parentInd = this.getParent(ind);
-            if (this.array[parentInd] > this.array[ind]) {
+            if (this.numbers[parentInd] > this.numbers[ind]) {
                 this.swap(parentInd, ind);
                 ind = parentInd;
             } else {
@@ -56,18 +56,18 @@ export default class MinHeap {
     // If the current value is greater than its smallest child, it does not satisfy the min heap
     // Requirment. Swap it with the child and move down the tree. Otherwise, we're done.
     minHeapifyDown() {
-        const lastInd = this.array.length - 1;
+        const lastInd = this.numbers.length - 1;
         for (let ind = 0; ind < lastInd;) {
             let [leftInd, rightInd] = this.getChildren(ind);
             let childInd = leftInd;
 
             if (leftInd > lastInd) {
                 return;
-            } else if (rightInd <= lastInd && this.array[rightInd] < this.array[leftInd]) {
+            } else if (rightInd <= lastInd && this.numbers[rightInd] < this.numbers[leftInd]) {
                 childInd = rightInd;
             }
 
-            if (this.array[ind] > this.array[childInd]) {
+            if (this.numbers[ind] > this.numbers[childInd]) {
                 this.swap(ind, childInd);
                 ind = childInd;
             } else {
@@ -78,7 +78,7 @@ export default class MinHeap {
 
     // ES6 destructure assignment that swaps two variables.
     swap(indA: number, indB: number) {
-        [this.array[indA], this.array[indB]] = [this.array[indB], this.array[indA]];
+        [this.numbers[indA], this.numbers[indB]] = [this.numbers[indB], this.numbers[indA]];
     }
 
     // Calculation to get parent index
