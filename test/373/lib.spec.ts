111544e007f9b1d1f360aa22b8206031f362b962
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-03 16:10:15 +0430

    1

--- test/373/lib.spec.ts
+++ test/373/lib.spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import func from '../../src/373/lib';
 
 suite('Testing 373 ...', () => {
-  test.only('case 1', async () => {
+  test('case 1', async () => {
     expect(func([1,7,11],[2,4,6], 3)).to.deep.equal([[1,2],[1,4],[1,6]]);
     // expect(func([1], 1)).to.deep.equal([1]);
   });
