8c421a04a294267cac7970b35447d3f71cebfbe6
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-03 17:56:36 +0430

    929

---
+++ test/929/lib.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+
+import func from '../../src/929/lib';
+
+suite('Testing 929 ...', () => {
+  test.only('case 1', async () => {
+
+    expect(func( [
+      'test.email+alex@leetcode.com',
+      'test.e.mail+bob.cathy@leetcode.com',
+      'testemail+david@lee.tcode.com',
+    ])).to.deep.equal(2);
+  });
+});
