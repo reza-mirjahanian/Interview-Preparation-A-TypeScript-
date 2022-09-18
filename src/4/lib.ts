b8789003f35ca3a800b11e6d938163a637206a5d
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-18 08:59:27 +0430

    5

--- src/4/lib.ts
+++ src/4/lib.ts
@@ -1,14 +1,46 @@
-// 3. Longest Substring Without Repeating Characters
-// https://stackoverflow.com/questions/8269916/what-is-sliding-window-algorithm-examples
-export default function func(s: string): number {
-  const table = new Map();
-  let max = 0,
-    i = 0,
-    j = 0;
-  while (j < s.length) {
-    i = table.has(s[j]) ? Math.max(table.get(s[j]) + 1, i) : i;
-    table.set(s[j], j++);
-    max = Math.max(j - i, max);
+// 4. Median of Two Sorted Arrays
+export default function findMedianSortedArrays(
+  nums1: number[],
+  nums2: number[]
+): number {
+  const len1 = nums1.length;
+  const len2 = nums2.length;
+  const requiredMoves = Math.floor((len1 + len2) / 2) + 1;
+
+  let cur: number;
+  let indA = 0;
+  let indB = 0;
+
+  if (len2 === 0 || (len1 !== 0 && nums1[0] < nums2[0])) {
+    cur = nums1[0];
+    indA = 1;
+  } else {
+    cur = nums2[0];
+    indB = 1;
   }
-  return max;
+
+  let prev = 0;
+
+  for (let i = 1; i < requiredMoves; i += 1) {
+    if (
+      (indA < len1 && indB < len2 && nums1[indA] < nums2[indB]) ||
+      (indA < len1 && indB >= len2)
+    ) {
+      prev = cur;
+      cur = nums1[indA++];
+    } else {
+      prev = cur;
+      cur = nums2[indB++];
+    }
+  }
+
+  if (cur + prev === 0) {
+    return 0;
+  }
+
+  if ((len1 + len2) % 2 === 0) {
+    return (cur + prev) / 2;
+  } else {
+    return cur;
+  }
 }
--- src/5/lib.ts
+++ src/5/lib.ts
@@ -1,46 +1,34 @@
-// 4. Median of Two Sorted Arrays
-export default function findMedianSortedArrays(
-  nums1: number[],
-  nums2: number[]
-): number {
-  const len1 = nums1.length;
-  const len2 = nums2.length;
-  const requiredMoves = Math.floor((len1 + len2) / 2) + 1;
+// 5. Longest Palindromic Substring
+export default function longestPalindrome(s: string): string {
+  let longest = '';
+  let palindrome = s[0];
+  for (let i = 0; i < s.length; i++) {
+    palindrome = s[i];
 
-  let cur: number;
-  let indA = 0;
-  let indB = 0;
+    let k = i;
+    while (s[k + 1] == s[i]) {
+      palindrome += s[k + 1];
+      k++;
+    }
 
-  if (len2 === 0 || (len1 !== 0 && nums1[0] < nums2[0])) {
-    cur = nums1[0];
-    indA = 1;
-  } else {
-    cur = nums2[0];
-    indB = 1;
-  }
+    i = k;
 
-  let prev = 0;
-
-  for (let i = 1; i < requiredMoves; i += 1) {
-    if (
-      (indA < len1 && indB < len2 && nums1[indA] < nums2[indB]) ||
-      (indA < len1 && indB >= len2)
+    let lowerIndex = i - palindrome.length,
+      upperIndex = i + 1;
+    while (
+      lowerIndex >= 0 &&
+      upperIndex < s.length &&
+      s[lowerIndex] === s[upperIndex]
     ) {
-      prev = cur;
-      cur = nums1[indA++];
-    } else {
-      prev = cur;
-      cur = nums2[indB++];
+      palindrome = s[lowerIndex] + palindrome + s[upperIndex];
+      lowerIndex--;
+      upperIndex++;
+    }
+
+    if (palindrome.length > longest.length) {
+      longest = palindrome;
     }
   }
 
-  if (cur + prev === 0) {
-    return 0;
-  }
-
-  if ((len1 + len2) % 2 === 0) {
-    return (cur + prev) / 2;
-  } else {
-    return cur;
-  }
+  return longest;
 }
